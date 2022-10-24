fc73e91bdefab0540036eede94891bfd9c3d26d3
Peachka <podcolodnik@gmail.com>
2022-10-24 11:57:37 +0300

    final result

---
+++ CMakeFiles/CLearn.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CLearn.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/CLearn.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/CLearn.dir/main.cpp.o: /home/anastasia/Lab3_AK/main.cpp \
+ /usr/include/stdc-predef.h /home/anastasia/Lab3_AK/foo.h
