a4a38dd8ff3fb1dc44201436914c7cb2ab7adbb1
Peachka <podcolodnik@gmail.com>
2022-10-25 13:14:14 +0300

    deleted excessive files

--- CMakeFiles/CLearn.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CLearn.
-# This may be replaced when dependencies are built.
--- CMakeFiles/CLearn.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CLearn.dir/main.cpp.o: /home/anastasia/Lab3_AK/main.cpp \
- /usr/include/stdc-predef.h /home/anastasia/Lab3_AK/foo.h
